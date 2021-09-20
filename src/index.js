fdcc5c69ab3c2dfdc8a15df74dec780ca4473a87
salmankhh8 <62678675+salmankhh8@users.noreply.github.com>
2021-09-21 05:05:40 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+// const citrus = ["lime", "Lemon", "Orange"];
+// const fruits = ["Apple", "Banana", "Coconut", ...citrus]; // es6 spread operator
+// console.log(fruits);
+
+// const fullName = {
+//   fName: "james",
+//   lName: "bond"
+// };
+// const user = {
+//   ...fullName,
+//   id: 1,
+//   username: "james007"
+// };
+// console.log(user);
